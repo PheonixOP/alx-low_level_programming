7cdda9501c250bed61c953da797ba691b423a5dc
PheonixOP <77975843+PheonixOP@users.noreply.github.com>
2023-02-17 15:44:51 -0800

    Print out all numbers smaller than 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Print all single digit numbers
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	printf("%d", n);
+	putchar('\n');
+
+	return (0);
+}
