f1d03637489b1fc7a8d4b73d61a12a9981586989
PheonixOP <77975843+PheonixOP@users.noreply.github.com>
2023-02-17 16:48:15 -0800

    Print the smallest combination of 3 digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Print the smallest combination of 3 digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ones;
+	int tens;
+	int hundreds;
+
+	for (hundreds = '0'; hundreds <= '9'; hundreds++)
+	{
+		for (tens = (hundreds + 1); tens <= '9'; tens++)
+		{
+			for (ones = (tens + 1); ones <= '9'; ones++)
+			{
+				putchar(hundreds);
+				putchar(tens);
+				putchar(ones);
+				if (hundreds != '7' || tens != '8' || ones != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
